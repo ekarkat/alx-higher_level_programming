a278bad6b96cb83a3fe3f1e9f906b890a16502d7
ekarkat <ekarkat@gmail.com>
2023-06-08 22:52:17 +0100

    tas6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    list = sys.argv[1:]
+    argc = len(list)
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+
+    a = int(list[0])
+    b = int(list[2])
+    op = list[1]
+
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == "+":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == "+":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit (1)
