4cb3e3b6cf1baa1f7b5da7430156e7bf4460230a
ekarkat <ekarkat@gmail.com>
2023-06-12 19:25:52 +0100

    tas8.9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    m_r = (len(sentence), sentence[0])
+    return (m_r)
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
+max_integer = __import__('9-max_integer').max_integer
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+my_list = [10165, 90, 2, 153, 34, 5, -13, 1015]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
