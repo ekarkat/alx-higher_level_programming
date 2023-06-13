47c6fb6b697e47680fc8157f73a721d01e3ca1e1
ekarkat <ekarkat@gmail.com>
2023-06-13 01:13:09 +0100

    tasl

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if the list is_palindrome
+ * @head: list head
+ * Return: return 1 if is_palindrome otherwise 0
+*/
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *first = *head;
+	listint_t *temp = *head;
+	int ln = 0, i = 0, j = 0;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		ln++;
+	}
+	while (i < (ln / 2))
+	{
+		listint_t *last = *head;
+
+		for (j = 0; j < ln - i - 1; j++)
+			last = last->next;
+		if (first->n == last->n)
+			first = first->next;
+		else
+			return (1);
+		i++;
+	}
+	return (1);
+}
