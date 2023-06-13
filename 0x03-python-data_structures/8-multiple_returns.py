abab4db14bef62f29a74a324e76a9f2cc952b67e
ekarkat <ekarkat@gmail.com>
2023-06-13 02:23:52 +0100

    aze

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    m_r = (len(sentence), sentence[0] if not sentence else None)
+    m_r = (len(sentence), sentence[0] if sentence else None)
     return (m_r)
