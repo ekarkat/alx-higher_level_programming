58f75e28839ab1b4f755955df1b4ed182d3ddf36
ekarkat <ekarkat@gmail.com>
2023-06-15 00:40:48 +0100

    aas100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,6 @@ def weight_average(my_list=[]):
         return 0
     moy, weight = 0, 0
     for lis in my_list:
-        moy = lis[0] * lis[1]
-        weight = lis[1]
+        moy += lis[0] * lis[1]
+        weight += lis[1]
     return (moy / weight)
