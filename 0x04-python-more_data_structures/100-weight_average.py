371eaf7f97dd6ab49230ecf18e8e081f25d32cf1
ekarkat <ekarkat@gmail.com>
2023-06-15 00:39:51 +0100

    ta100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    moy, weight = 0, 0
+    for lis in my_list:
+        moy = lis[0] * lis[1]
+        weight = lis[1]
+    return (moy / weight)
