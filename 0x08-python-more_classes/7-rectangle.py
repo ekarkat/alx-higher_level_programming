79f32aad0670e36723e65c8023cbb51f9b237388
ekarkat <ekarkat@gmail.com>
2023-07-04 02:03:35 +0100

    task 8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -75,14 +75,3 @@ class Rectangle ():
         """ Deletes  object """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """compares rectangles"""
-        if type(rect_1) is not Rectangle:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() <= rect_2.area():
-            return rect_2
-        return rect_1
