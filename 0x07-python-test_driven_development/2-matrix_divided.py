a9b4bf3f7ebe435a850286aff6f3151118393c45
ekarkat <ekarkat@gmail.com>
2023-07-04 14:22:38 +0100

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,24 +2,32 @@
 
 
 def matrix_divided(matrix, div):
-    if len(matrix) == 0 or len(matrix[0]) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """Check type of matrix"""
+    type_error = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(type_error)
+    if len(matrix) == 0:
+        raise TypeError(type_error)
+    for line in matrix:
+        if type(line) is not list:
+            raise TypeError(type_error)
+
     """check if each element of mattrix is an int"""
+    for line in matrix:
+        if type(line) is not list or len(line) == 0:
+            raise TypeError(type_error)
     for line in matrix:
         for n in line:
             if type(n) is not int and type(n) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for line in matrix:
-        if type(line) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(type_error)
+
+    """Check that each list of matrix has the same size"""
     size_line = len(matrix[0])
     for i in range(len(matrix)):
         if len(matrix[i]) != size_line:
             raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, (int, float)):
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
