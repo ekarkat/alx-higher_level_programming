0e9fa54b8e6051bc0ae79b93586790b93401bc21
ekarkat <ekarkat@gmail.com>
2023-07-10 23:22:59 +0100

    task2.3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module 3"""
+
+
 def is_kind_of_class(obj, a_class):
     '''Check if the object is exactly an instance of the specified class'''
     if isinstance(obj, a_class):
