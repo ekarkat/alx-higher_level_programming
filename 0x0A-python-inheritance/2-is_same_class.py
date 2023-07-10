3ace8444ab0aa0d6afe2e860d26edf3e43dc5634
ekarkat <ekarkat@gmail.com>
2023-07-10 23:30:15 +0100

    task2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module 2"""
+'''Module2'''
 
 
-def is_kind_of_class(obj, a_class):
-    '''Check if the object is exactly an instance of the specified class'''
+def is_same_class(obj, a_class):
+    '''Check if obj is instance of a_class'''
     if type(obj) == a_class:
         return True
     return False
