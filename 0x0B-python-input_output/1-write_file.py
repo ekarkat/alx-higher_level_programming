c3c0ed872222c1a1980cc835f66fa65df45384bd
ekarkat <ekarkat@gmail.com>
2023-07-11 15:09:17 +0100

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module 1'''
+
+
+def write_file(filename="", text=""):
+    '''Function to write the content of a file'''
+    with open(filename, 'w', encoding="utf-8") as f:
+        return(f.write(text))
