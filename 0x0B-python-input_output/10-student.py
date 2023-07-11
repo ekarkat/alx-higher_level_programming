77dac45140ff516267c405b7030e31bb4b2c6d32
ekarkat <ekarkat@gmail.com>
2023-07-11 19:27:46 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Module 6'''
+
+
+class Student():
+    """Class Student"""
+
+    first_name = ""
+    last_name = ""
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            for key in attrs:
+                if type(key) is str:
+                    continue
+                return self.__dict__
+            liste = {}
+            for key in attrs:
+                if key in self.__dict__.keys():
+                    liste[key] = self.__dict__[key]
+            return liste
+        else:
+            return self.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module 6'''
 
+
 class Student():
     """Class Student"""
 
