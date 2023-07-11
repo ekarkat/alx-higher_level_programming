7db534029fc1ddb8b6fb37872a40d97195fa237c
ekarkat <ekarkat@gmail.com>
2023-07-11 15:01:09 +0100

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     '''Function to print the content of a file'''
     with open(filename, encoding="utf-8") as f:
         f_content = f.read()
-        print(f_content)
+        print(f_content, end="")
