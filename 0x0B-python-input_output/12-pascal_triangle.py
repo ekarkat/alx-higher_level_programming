9033dc476351a3fe3dfcd9ef4e452a17cb87a3f5
ekarkat <ekarkat@gmail.com>
2023-07-12 01:41:56 +0100

    ras12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python3
+"""Moduile 12"""
 def factorial(n):
+    """Factorial function"""
     if n <= 1:
         return 1
     return n * factorial(n - 1)
 
 def pascal_triangle(n):
+    """Pascal function"""
     if n <= 0:
         return []
     triangle = []
