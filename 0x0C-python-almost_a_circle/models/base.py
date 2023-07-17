86dff1e98bad26d494ad077bf504cdad60326297
ekarkat <ekarkat@gmail.com>
2023-07-17 23:59:32 +0100

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,14 @@ class Base():
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save to json file"""
+        file_name = cls.__name__ + ".json"
+        list_dict = []
+        if list_objs is not None:
+            for item in list_objs:
+                list_dict.append(item.to_dictionary())
+        with open(file_name, "w") as f:
+            f.write(Base.to_json_string(list_dict))
