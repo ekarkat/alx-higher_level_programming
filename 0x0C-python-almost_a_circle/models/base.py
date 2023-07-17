8a73dfb26f7cd52ce56d4b04e03ac748c7927cfd
ekarkat <ekarkat@gmail.com>
2023-07-18 00:11:20 +0100

    edit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,5 +39,5 @@ class Base():
     def from_json_string(json_string):
         """Load from json string"""
         if json_string is None or json_string == []:
-            return '[]'
+            return []
         return json.loads(json_string)
