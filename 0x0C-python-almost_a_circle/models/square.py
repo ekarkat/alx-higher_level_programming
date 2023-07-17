bec66f768ffffb4727b104053497119f35e08398
ekarkat <ekarkat@gmail.com>
2023-07-17 22:28:39 +0100

    task update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """Return size"""
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -22,8 +22,8 @@ class Square(Rectangle):
             raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError('width must be > 0')
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """print str"""
@@ -32,3 +32,21 @@ class Square(Rectangle):
                                                   self.y,
                                                   self.width)
         return outpt
+
+    def update(self, *args, **kwargs):
+        """update Square"""
+        if len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                elif i == 1:
+                    self.width = args[1]
+                elif i == 2:
+                    self.height = args[1]
+                elif i == 3:
+                    self.x = args[2]
+                elif i == 4:
+                    self.y = args[3]
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                self.__setattr__(key, value)       
