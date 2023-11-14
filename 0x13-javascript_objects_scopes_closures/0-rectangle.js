5cc4695be6d852d5fa1e2673673f38079d58f134
ekarkat <ekarkat@gmail.com>
2023-11-14 19:37:39 +0000

    task 0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-//empty class Rectangle
-module.exports = class Rectangle {};
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
