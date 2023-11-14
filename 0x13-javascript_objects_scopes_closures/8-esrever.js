8138f860354ddc19d183fc8a324c1ea717e02bbd
ekarkat <ekarkat@gmail.com>
2023-11-14 21:28:06 +0000

    tas8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-	let rev = [];
-	let li = list;
-	const l = list.length
-	for (let i = 0; i < l; i++) {
-		rev.push(li.pop());
-	}
-	return (rev);
-}
+  const rev = [];
+  const li = list;
+  const l = list.length;
+  for (let i = 0; i < l; i++) {
+    rev.push(li.pop());
+  }
+  return (rev);
+};
