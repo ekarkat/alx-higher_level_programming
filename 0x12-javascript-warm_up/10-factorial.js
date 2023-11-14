27c9a805569129a8aca662adda35250201003ec6
ekarkat <ekarkat@gmail.com>
2023-11-14 01:19:02 +0000

    tas10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,8 @@
 function factorial (n) {
   let fac = 1;
   for (let i = 1; i < n + 1; i++) {
-    if (n === 0) {
-      return (1);
-    }
     fac = fac * i;
-    return (fac);
   }
+  return (fac);
 }
 console.log(factorial(parseInt(process.argv[2])));
