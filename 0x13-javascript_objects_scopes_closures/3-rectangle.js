1783cf3b0d0f886fc33e68f35749347c34fe0c26
ekarkat <ekarkat@gmail.com>
2023-11-14 19:50:23 +0000

    tas3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let figure = '';
+    for (let i = 0; i < this.width; i++) {
+      figure = figure + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(figure);
+    }
+  }
+};
