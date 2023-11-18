f4f8705acf7a37bf9bf95f2faaa63c38fa4088d0
ekarkat <ekarkat@gmail.com>
2023-11-18 18:48:47 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Sess = sessionmaker(bind=engine)
+    sess = Sess()
+
+    res = sess.query(State).order_by(State.id).filter(State.name.like("%a%"))
+
+    for x in res:
+        sess.delete(x)
+
+    sess.commit()
