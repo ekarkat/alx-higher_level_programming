5ba8c11c20fd9f18024776e29a090f756a968eea
ekarkat <ekarkat@gmail.com>
2023-11-18 18:01:07 +0000

    tasl8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.query(State).order_by(State.id).all()
+    result = session.query(State).order_by(State.id).first()
 
     if not result:
         print("Nothing")
     else:
-        print("{}: {}".format(result[0].id, result[0].name))
+        print("{}: {}".format(result.id, result.name))
