c2a35180dae03cd1522e05b67eba2710687acbdc
ekarkat <ekarkat@gmail.com>
2023-11-30 23:29:21 +0000

    tas6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,14 +8,11 @@ def find_peak(list_of_integers):
         return None
     elif len(list_of_integers) == 1:
         return list_of_integers[0]
-    else:
-        i = 0
-        pik = list_of_integers[1]
-        lis = list_of_integers
-        while i < (len(list_of_integers) - 1):
-            if lis[i] >= lis[i + 1]:
-                pik = lis[i]
-            else:
-                pik = lis[i + 1]
-            i += 1
-    return (pik)
+
+    i = 1
+    li = list_of_integers
+    while i < len(list_of_integers) - 1:
+        if li[i] > li[i - 1] and li[i] > li[i + 1]:
+            return (li[i])
+        i += 1
+    return (max(list_of_integers))
