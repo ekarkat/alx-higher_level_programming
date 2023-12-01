0dc7c9fb6f5f7654a87920b734f6ee8dba12f7d5
ekarkat <ekarkat@gmail.com>
2023-12-01 23:38:05 +0000

    ta8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""model n 0"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        values = {'q': "{}".format(argv[1])}
+    except IndexError:
+        values = {"q": ""}
+    response = requests.post(url, data=values)
+    content_type = response.headers.get('Content-Type')
+    # print(content_type)
+
+    if (content_type is not None
+            and 'application/json' in content_type):
+        data = eval(response.text)
+        if not data:
+            print("No result")
+        else:
+            print("[{}] {}".format(data["id"], data["name"]))
+    else:
+        print("Not a valid JSON")
