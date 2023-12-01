dbdf3f783389f50a0ff9d3ee409d7d820aadfb69
ekarkat <ekarkat@gmail.com>
2023-12-01 21:18:37 +0000

    gag6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,5 @@ import requests
 if __name__ == "__main__":
     url = argv[1]
     values = {"email": "{}".format(argv[2])}
-    
     response = requests.post(url, data=values)
     print("{}".format(response.text))
