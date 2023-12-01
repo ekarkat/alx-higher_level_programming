58f3d46faebae183edac738d6a5e95d477da9a7c
ekarkat <ekarkat@gmail.com>
2023-12-01 20:17:11 +0000

    ta1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,12 @@
 """model n 0"""
 from urllib import request, error, response, parse, robotparser
 
-req = request.Request("https://alx-intranet.hbtn.io/status")
-with request.urlopen(req) as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format((html)))
-    print("\t- utf8 content: {}".format((html.decode("UTF-8"))))
+
+if __name__ == "__main__":
+    req = request.Request("https://alx-intranet.hbtn.io/status")
+    with request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format((html)))
+        print("\t- utf8 content: {}".format((html.decode("UTF-8"))))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 from urllib import request, error, response, parse, robotparser
 from sys import argv
 
-
-req = request.Request(argv[1])
-with request.urlopen(req) as response:
-    print((response.getheader('X-Request-Id')))
+if __name__ == "__main__":
+    req = request.Request(argv[1])
+    with request.urlopen(req) as response:
+        print((response.getheader('X-Request-Id')))
