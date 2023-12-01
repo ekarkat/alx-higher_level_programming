9d61ee3f73ff9c042506680a885de2f79a32c136
ekarkat <ekarkat@gmail.com>
2023-12-01 21:18:02 +0000

    ta6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""model n 0"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {"email": "{}".format(argv[2])}
+    
+    response = requests.post(url, data=values)
+    print("{}".format(response.text))
