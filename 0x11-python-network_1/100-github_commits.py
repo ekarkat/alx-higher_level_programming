d27f658ccbb6228fb107a5a4d0722525a63e4a42
ekarkat <ekarkat@gmail.com>
2023-12-02 01:00:06 +0000

    ta100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     data = response.json()
 
     try:
-        for i in range(10):
-            print("{}: {}".format(data[i].get("sha"), data[i].get(
-                        "commit").get("author").get("name")))
+        response = requests.get(url)
+        data = response.json()
+        for i in range(0, 10):
+            print("{}: {}".format(data[i].get('sha'), data[i].get(
+                'commit').get('author').get('name')))
     except Exception:
         pass
