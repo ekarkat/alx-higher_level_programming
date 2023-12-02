2755cb5cacbe8189891ac2eb7c908eb27bfa9500
ekarkat <ekarkat@gmail.com>
2023-12-02 00:48:09 +0000

    ta100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
     response = requests.get(url)
     data = response.json()
 
-    for i in range(10):
-        print("{}: {}".format(data[i].get("sha"),
-                              data[i].get("commit").get("author").get("name")))
+    try:
+        for i in range(10):
+            print("{}: {}".format(data[i].get("sha"), data[i].get(
+                        "commit").get("author").get("name")))
+    except Exception:
+        pass
