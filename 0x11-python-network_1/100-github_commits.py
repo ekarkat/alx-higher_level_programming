d168342e5b3d22ce6eed6419a5b810b3c4630c29
ekarkat <ekarkat@gmail.com>
2023-12-02 00:27:33 +0000

    tas100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    pass
+    rp_nme = argv[2]
+    ow_nme = argv[1]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(ow_nme, rp_nme)
+    head = {"Authorization": "token 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"}
+
+    response = requests.get(url, headers=head)
+    data = response.json()
+    for i in range(10):
+        print("{} {}".format(data[i].get("sha"),
+                             data[i].get("commit").get("author").get("name")))
