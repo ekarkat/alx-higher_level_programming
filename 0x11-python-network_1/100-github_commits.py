200a993ce21c5738414f908ec0ce3bd02f463899
ekarkat <ekarkat@gmail.com>
2023-12-02 01:01:56 +0000

    ta100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,17 +5,15 @@ from sys import argv
 import requests
 
 if __name__ == '__main__':
-    repo = argv[2]
-    owner = argv[1]
+    repo = argv[1]
+    owner = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
     response = requests.get(url)
     data = response.json()
 
     try:
-        response = requests.get(url)
-        data = response.json()
-        for i in range(0, 10):
-            print("{}: {}".format(data[i].get('sha'), data[i].get(
-                'commit').get('author').get('name')))
+        for i in range(10):
+            print("{}: {}".format(data[i].get("sha"), data[i].get(
+                        "commit").get("author").get("name")))
     except Exception:
         pass
