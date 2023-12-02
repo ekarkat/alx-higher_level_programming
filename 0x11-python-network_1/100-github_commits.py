100fa9fcf6676a670bfcd8d20f1792cc9f8e15e6
ekarkat <ekarkat@gmail.com>
2023-12-02 00:39:30 +0000

    ta10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""back-end test"""
+"""back end test"""
+
 from sys import argv
 import requests
 
 if __name__ == "__main__":
     pass
-    rp_nme = argv[2]
-    ow_nme = argv[1]
-
-    url = "https://api.github.com/repos/{}/{}/commits".format(ow_nme, rp_nme)
-    head = {"Authorization": "token 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"}
-
-    response = requests.get(url, headers=head)
+    repo = argv[2]
+    owner = argv[1]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    response = requests.get(url)
     data = response.json()
+
     for i in range(10):
-        print("{} {}".format(data[i].get("sha"),
-                             data[i].get("commit").get("author").get("name")))
+        print("{}: {}".format(data[i].get("sha"),
+                              data[i].get("commit").get("author").get("name")))
