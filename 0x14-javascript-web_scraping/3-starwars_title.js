f30cc84575321caeebe9b91438571e81daa3f2be
ekarkat <ekarkat@gmail.com>
2024-01-03 22:56:53 +0000

    tas3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, function (_error, { response }, body) {
+  const result = JSON.parse(body);
+  console.log(result.title);
+});
