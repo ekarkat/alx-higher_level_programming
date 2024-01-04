9c4aef51d17ba49ac703744289762127778d7c7a
ekarkat <ekarkat@gmail.com>
2024-01-04 00:29:33 +0000

    tas1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
-const { writeFile } = require('fs');
+const { readFile } = require('fs');
 
 const fileName = process.argv[2];
-const string = process.argv[3];
 
-writeFile(fileName, string, function (error) {
-  if (error) console.log(error);
+readFile(fileName, 'utf-8', (err, content) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(content);
 });
